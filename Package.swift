463178565a366e181ede102724bfba1424bf57de
jkalamarz-survicate <jakub.k@survicate.com>
2023-01-13 09:30:27 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Survicate",
-            url: "https://repo.survicate.com/ios/1.8.7/Survicate.zip",
-            checksum: "40a0f1272ff288667994ed00501e239ecec242ca2eee440ee0cc803cd78444c0"),
+            url: "https://repo.survicate.com/ios/1.8.8/Survicate.zip",
+            checksum: "7b1acd07076e2ac4ca443e288ca1c31ac8fb81f1ded2ce11fba159e5ad76ee78"),
     ]
 )
