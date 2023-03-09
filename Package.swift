2682cb2a30f1b148766fa08fddbd8749126d5052
jkalamarz-survicate <jakub.k@survicate.com>
2023-03-09 15:15:48 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -14,7 +14,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Survicate",
-            url: "https://repo.survicate.com/ios/1.8.10/Survicate.zip",
-            checksum: "90b7ae8537fad5cecaed9888185a7a0ce150d77313acbf34a33901ff9d19f89e"),
+            url: "https://repo.survicate.com/ios/2.0.0/Survicate.zip",
+            checksum: "d7ec17cdb8d25a633f6d42224e93b630432f4e56e4874af74a10fafb0a7a5e43"),
     ]
 )
