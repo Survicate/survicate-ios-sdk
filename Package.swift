9c57fa7d98de964a943fc37c8f76384a68ecce61
Jarek Brzozowski <45065772+j-brzozowski@users.noreply.github.com>
2024-12-20 09:42:02 +0100

    Update Package.swift with new version and checksum. (#28)
    Co-authored-by: jkalamarz-survicate <jkalamarz-survicate@users.noreply.github.com>

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Survicate",
-            url: "https://repo.survicate.com/ios/5.5.1/Survicate.zip",
-            checksum: "e67ef06896e4469745c2f3daa71fcfd7a75bf6d698a471a6d490b5084a387b5a"),
+            url: "https://repo.survicate.com/ios/5.5.2/Survicate.zip",
+            checksum: "146a1a9c50e1e22f17c553ca8080ff9f956716800288e6655ecf12c30091c076"),
     ]
 )
